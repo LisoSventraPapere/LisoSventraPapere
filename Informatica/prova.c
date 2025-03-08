298cf664a199dc1683e6f9dfaf5d7df35cfcec23
LisoSventraPapere <alebel1707@gmail.com>
2025-03-08 12:13:20 +0000

    forse ora funziona

--- Informatica/prova.c
+++ Informatica/prova.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
-void cambia(int x) {
-    x = x + 10;
+void cambia(int *x) {
+    *x = *x + 10;
 }
 
 int main() {
     int numero = 5;
-    cambia(numero);
+    cambia(&numero);
     printf("Numero: %d\n", numero);
     return 0;
 }
+
